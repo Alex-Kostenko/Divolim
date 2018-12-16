cf900f4a116fe7c79b84143fedcfd69f40f05259
Alex-Kostenko <0984181760ss@gmail.com>
2018-12-16 22:44:23 +0200

    js

---
+++ js/main.js
@@ -0,0 +1,78 @@
+function showGorizontal() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showGorizontal').style.display="flex";
+}
+function showAlign() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showAlign').style.display="flex";
+}
+function showPlisse() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showPlisse').style.display="flex";
+}
+function showRim() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showRim').style.display="flex";
+}
+function showBamb() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showBamb').style.display="flex";
+}
+function showAnti() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showAnti').style.display="flex";
+}
+function showRoletu() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showRoletu').style.display="flex";
+}
+function showOkna_dv() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showOkna_dv').style.display="flex";
+}
+/**********************************/
+function showUniversal() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showUniversal').style.display="flex";
+}
+function showOpen() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showOpen').style.display="flex";
+}
+function showClose() {
+  let x = document.getElementsByClassName("wrap_prod");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById('showClose').style.display="flex";
+}
